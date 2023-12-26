ecd19b930f0288b08c55a8952371ba4744ffc703
“vikramreddy2662” <vikramreddy2662@gmail.com>
2023-12-26 22:34:22 +0530

    AngularServices With Dependency Injection

--- src/app/header/home/hero/hero.component.ts
+++ src/app/header/home/hero/hero.component.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core';
-import { Service } from './../../../Services/Services.service' ;
+import { Service } from './../../../Services/Services.service';
 
 @Component({
   selector: 'app-hero',
-  templateUrl: './hero.component.html'
+  templateUrl: './hero.component.html',
+  providers: [Service]
 })
 export class HeroComponent {
-  OnSubscribe(){
 
- 
-    
-    let subService=new Service();
-    subService.OnSubscribeClicked('yearly');
+  constructor(private subService:Service){
+
+  }
+
+  OnSubscribe() {
+
+    //let subService = new Service();
+    this.subService.OnSubscribeClicked('yearly');
 
 
   }
--- src/app/header/home/sidebar/sidebar.component.ts
+++ src/app/header/home/sidebar/sidebar.component.ts
@@ -4,16 +4,17 @@ import { Service } from './../../../Services/Services.service' ;
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  providers:[Service]
 })
 export class SidebarComponent {
+
+  constructor(private subService:Service){}
  
   OnSubscribe(){
 
- 
-    
-    let subService=new Service();
-    subService.OnSubscribeClicked('daily');
+//let subService=new Service();
+    this.subService.OnSubscribeClicked('daily');
 
 
   }
