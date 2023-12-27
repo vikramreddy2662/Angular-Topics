24fa5d6e6e8ff9b6731470c3473c10e4d20a51e6
“vikramreddy2662” <vikramreddy2662@gmail.com>
2023-12-27 22:40:53 +0530

    ObservablesEmittingDataAtOnce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Observables';
+
+  data: any[] = [];
+
+  //creating observables
+
+  myObservable = new Observable((observer) => {
+
+    observer.next([1, 21, 34, 53, 21, 56]);
+
+  });
+
+  //observables emits 3 types of events next error complete. emits data as call back.
+
+  //observer also takes or recieves 3 types of callbacks as data next,error & complete.recieves data as call back 
+  GetAsyncData() {
+
+    this.myObservable.subscribe((val: any) => {
+
+      this.data = val;
+
+
+    });
+
+  }
 }
