c77d199271af5152bb250d7d2cbac6df3481a5c7
“vikramreddy2662” <vikramreddy2662@gmail.com>
2023-12-28 10:38:40 +0530

    Observers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { error } from 'console';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -17,12 +18,18 @@ export class AppComponent {
 
     // observer.next([1, 21, 34, 53, 21, 56]); to emit whole data at once.
 
-   setTimeout(()=>{ observer.next(11)},1000);
-   setTimeout(()=>{ observer.next(43)},2000)
-   setTimeout(()=>{ observer.next(87)},3000)
-   setTimeout(()=>{ observer.next(96)},5000)
-   setTimeout(()=>{ observer.next(1)},7000)
-   setTimeout(()=>{ observer.next(56)},6000)
+    setTimeout(() => { observer.next(11) }, 1000);
+    setTimeout(() => { observer.next(43) }, 2000)
+    setTimeout(() => { observer.next(87) }, 3000)
+    setTimeout(() => { observer.error(new Error('Something went wrong please try again later')) }, 5000)//after 3 seconds execution gets stops error message gets displayed.
+    /*when we use error method and set time out data up to that that message will be displayed after errror method gets
+     executed be other nethod gets executed
+      */
+    setTimeout(() => { observer.next(96) }, 5000)
+    setTimeout(() => { observer.next(1) }, 7000)
+    setTimeout(() => { observer.complete() }, 4000)
+
+    //emmitting data in chunks 
 
 
   });
@@ -32,12 +39,33 @@ export class AppComponent {
   //observer also takes or recieves 3 types of callbacks as data next,error & complete.recieves data as call back 
   GetAsyncData() {
 
-    this.myObservable.subscribe((val: any) => {
+    /*this.myObservable.subscribe((val: any) => {
+
+      //this.data=val; to get whole data at once from array
 
       this.data.push(val);
+    },
 
+      (err) => {
+        alert(err.message);
+      },
+      () => {
 
+        alert('all the data is transferred');
+      });*/
+
+    this.myObservable.subscribe({
+      next: (val: any) => {
+        this.data.push(val);
+
+      },
+
+      error(err) {
+        alert(err.message);
+      },
+      complete() {
+        alert('all the data get streamed');
+      }
     });
-
   }
 }
